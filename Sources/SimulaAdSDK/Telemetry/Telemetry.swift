53b4d841dd84b7ba593525798acd5146239662d2
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2026-09-02 10:45:30 -0600

    Bump version for release

--- Sources/SimulaAdSDK/Telemetry/Telemetry.swift
+++ Sources/SimulaAdSDK/Telemetry/Telemetry.swift
@@ -8,7 +8,7 @@ import CoreTelephony
 
 /// SDK version stamped on every telemetry batch. Keep in sync with `SimulaAdSDK.podspec`
 /// (`s.version`) and the SPM release tag.
-let SIMULA_SDK_VERSION = "1.2.1-dev.2"
+let SIMULA_SDK_VERSION = "1.2.1"
 
 struct DuplicateInitializeCountBuffer {
     private(set) var count = 0
